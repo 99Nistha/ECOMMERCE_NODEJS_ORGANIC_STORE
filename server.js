79256f573c94fb3d95fc9395b4bc86f59eaf73ad
99Nistha <nisthaagarwal8@gmail.com>
2020-06-13 16:57:30 +0530

    Making folder and setting up server.js file

---
+++ server.js
@@ -0,0 +1,23 @@
+require('./models/db');
+const express= require('express');
+const path= require('path');
+const exphbs= require('express-handlebars');
+const bodyparser= require('body-parser');
+
+var app= express();
+app.use(bodyparser.urlencoded({
+    extended: true
+}))
+app.use(bodyparser.json());
+app.use(express.static(path.join(__dirname,'/public')));
+app.set('views',path.join(__dirname,'views'));
+app.emgine('hbs', exphbs({
+    extname:'hbs',
+    defaultLayout: 'mainLayout',
+    layoutsDir:__dirname+'/views/'
+}));
+app.set('view engine', 'hbs');
+app.listen(3000,()=>{
+    console.log('Server on port: 3000');
+});
+app.use('/', orederController);
